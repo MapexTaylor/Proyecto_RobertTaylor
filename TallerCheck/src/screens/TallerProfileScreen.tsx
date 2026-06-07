b1abf84a7ed388b050d330996dce826d55b50534
Robert Taylor <robert.taylor.mk@gmail.com>
2026-06-07 15:07:59 -0600

    - Creacion de Profile del taller. - Se muestra la informacion general del taller/mecanico. - Se agrego un boton para logout.

---
+++ TallerCheck/src/screens/TallerProfileScreen.tsx
@@ -0,0 +1,93 @@
+import { View, Text, StyleSheet, Image } from "react-native";
+import { navigationRef } from "../navigation/NavigationService";
+import CustomButton from "../components/CustomButton";
+
+export default function TallerProfileScreen() {
+
+  const handleLogout = () =>{
+        if(navigationRef.isReady()){
+            navigationRef.reset({
+                //inicie el arrelo routes, que indica la vista actual al momento de reset el stack de navegacion
+                index: 0,
+                //es un arreglo de objetos, para el cual cada objeto representa una ruta en el nuevo
+                routes: [{name: 'Login'}]
+            })
+        }
+    }
+  
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("../../assets/Mec_Icon.png")}
+        style={styles.image}
+      />
+
+      <Text style={styles.title}>Perfil del Taller</Text>
+
+      <View style={styles.card}>
+        <Text style={styles.label}>Nombre del taller:</Text>
+        <Text style={styles.value}>TallerCheck Servicio Técnico</Text>
+
+        <Text style={styles.label}>Encargado:</Text>
+        <Text style={styles.value}>Robert Taylor</Text>
+
+        <Text style={styles.label}>Correo:</Text>
+        <Text style={styles.value}>robert.taylor@tallercheck.com</Text>
+
+        <Text style={styles.label}>Teléfono:</Text>
+        <Text style={styles.value}>9999-9999</Text>
+
+        <Text style={styles.label}>Horario:</Text>
+        <Text style={styles.value}>Lunes a sábado, 8:00 a.m. - 5:00 p.m.</Text>
+      </View>
+
+      <CustomButton 
+      title={"Cerrar Sesión"} 
+      onPress={handleLogout}/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#eaeaea",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  image: {
+    width: 130,
+    height: 130,
+    resizeMode: "contain",
+    marginBottom: 15,
+  },
+  title: {
+    fontSize: 27,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  card: {
+    backgroundColor: "#fff",
+    width: "100%",
+    padding: 20,
+    borderRadius: 15,
+    borderWidth: 1,
+    borderColor: "#ccc",
+  },
+  label: {
+    fontSize: 15,
+    fontWeight: "bold",
+    marginTop: 10,
+  },
+  value: {
+    fontSize: 15,
+    color: "#333",
+  },
+  note: {
+    marginTop: 15,
+    fontSize: 13,
+    textAlign: "center",
+    color: "#555",
+  },
+});
