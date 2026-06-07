0a0b9fe38052db1da4bb0cb1177d742ba1f8b304
Robert Taylor <robert.taylor.mk@gmail.com>
2026-06-07 15:06:46 -0600

    - Creacion de Pantalla Historial. - Muestra una card con solo las ordenes entregadas, para que tenga un historial de todos los vehiculos completados.

---
+++ TallerCheck/src/screens/HistorialScreen.tsx
@@ -0,0 +1,87 @@
+import { ScrollView, View, Text, StyleSheet } from "react-native";
+import { ordersData } from "../data/ordersData";
+
+export default function HistoryScreen() {
+  const deliveredOrders = ordersData.filter(
+    (order) => order.status === "Entregado"
+  );
+
+  return (
+    <ScrollView style={styles.container}>
+      <Text style={styles.title}>Historial</Text>
+
+      <Text style={styles.subtitle}>
+        Reparaciones que ya fueron entregadas al cliente.
+      </Text>
+
+      {deliveredOrders.map((order) => (
+        <View key={order.id} style={styles.card}>
+          <View style={styles.cardHeader}>
+            <Text style={styles.code}>{order.code}</Text>
+            <Text style={styles.status}>{order.status}</Text>
+          </View>
+
+          <Text style={styles.text}>Cliente: {order.clientName}</Text>
+          <Text style={styles.text}>Teléfono: {order.phone}</Text>
+          <Text style={styles.text}>Vehículo: {order.marca}</Text>
+          <Text style={styles.text}>Problema: {order.problem}</Text>
+          <Text style={styles.date}>Fecha de ingreso: {order.entryDate}</Text>
+        </View>
+      ))}
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#eaeaea",
+    padding: 20,
+  },
+  title: {
+    fontSize: 27,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 8,
+  },
+  subtitle: {
+    fontSize: 15,
+    textAlign: "center",
+    marginBottom: 20,
+    color: "#333",
+  },
+  card: {
+    backgroundColor: "#fff",
+    padding: 15,
+    borderRadius: 12,
+    marginBottom: 15,
+    borderWidth: 1,
+    borderColor: "#ccc",
+  },
+  cardHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  code: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  status: {
+    backgroundColor: "#c8e6c9",
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 10,
+    fontWeight: "bold",
+  },
+  text: {
+    fontSize: 15,
+    marginBottom: 4,
+  },
+  date: {
+    fontSize: 14,
+    marginTop: 8,
+    color: "#555",
+  },
+});
