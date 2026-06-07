bc67967f9fa47b4a2d4f382f4838a31891127bf5
Robert Taylor <robert.taylor.mk@gmail.com>
2026-06-07 14:53:31 -0600

    - Implementacion de el NavigationService para poder moverse entre pantallas. - Creacion de StackNavigation para poder moverse entre las pantallas principales. "UserTabs" sera usado por el mecanico ya que solo "ClientSearch" es la pantalla para el cliente. - Creacion del TabsNavigation. El mecanico tendra cinco tabs "Inicio", "Ordenes", "Nueva","Historial" y "Taller"

---
+++ TallerCheck/src/navigation/StackNavigation.tsx
@@ -0,0 +1,31 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import LoginScreen from "../screens/LoginScreen";
+import TabsNavigation from "./TabsNavigation";
+import ClientSearchScreen from "../screens/ClientSearchScreen";
+import TallerLoginScreen from "../screens/TallerLoginScreen";
+import TallerHomeScreen from "../screens/TallerHomeScreen";
+
+
+
+export type RootStackParamList = {
+    Login: undefined;
+    TallerHome: undefined;
+    UserTabs: undefined;
+    ClientSearch: undefined;
+    TallerLogin: undefined;
+}
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function StackNavigator(){
+    return(
+        <Stack.Navigator initialRouteName="Login" screenOptions={{headerShown: false}}>
+            <Stack.Screen name="Login" component={LoginScreen}/>
+            <Stack.Screen name="TallerHome" component={TallerHomeScreen}/>
+            <Stack.Screen name="UserTabs" component={TabsNavigation}/>
+            <Stack.Screen name="ClientSearch" component={ClientSearchScreen}/>
+            <Stack.Screen name="TallerLogin" component={TallerLoginScreen}/>
+        </Stack.Navigator>
+    )
+}
+
---
+++ TallerCheck/src/navigation/TabsNavigation.tsx
@@ -0,0 +1,90 @@
+import { Ionicons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RootStackParamList } from "./StackNavigation";
+import HistorialScreen from "../screens/HistorialScreen";
+import TallerHomeScreen from "../screens/TallerHomeScreen";
+import OrdersScreen from "../screens/OrdersScreen";
+import RegisterOrderScreen from "../screens/RegisterOrderScreen";
+import TallerProfileScreen from "../screens/TallerProfileScreen";
+
+
+
+type TabsParamList={
+    Profile: undefined;
+    Ordenes: undefined;
+    Home: undefined;
+    Historial: undefined;
+    NuevaOrden: undefined;
+    Taller:undefined;
+}
+
+type Props = NativeStackScreenProps<RootStackParamList,"UserTabs">;
+
+const Tab = createBottomTabNavigator<TabsParamList>();
+
+export default function TabsNavigation(){
+    return(
+        <Tab.Navigator 
+        screenOptions={{tabBarActiveTintColor: '#1150af'}}>
+            <Tab.Screen
+            name="Home"
+            component={TallerHomeScreen}
+            options={{
+                title: "Inicio",
+                tabBarIcon:({color, size}) => (
+                    <Ionicons name="home" size={size} color={color}/>
+                ),    
+            }}
+            />
+
+            <Tab.Screen
+            name="Ordenes"
+            component={OrdersScreen}
+            options={{
+                title: "Ordenes",
+                tabBarIcon:({color, size}) => (
+                    <Ionicons name="document-text" size={size} color={color}/>
+                ),
+            }}
+            />
+
+            <Tab.Screen
+            name="NuevaOrden"
+            component={RegisterOrderScreen}
+            options={{
+                title: "Nueva",
+                tabBarIcon:({color, size}) => (
+                    <Ionicons name="add-circle" size={size} color={color}/>
+                ),
+            }}
+            />    
+
+
+            <Tab.Screen
+            name = "Historial"
+            component={HistorialScreen}
+            options={{
+                title: "Historial",
+                tabBarIcon:({color, size}) => (
+                    <Ionicons name="time" size = {size} color = {color}/>
+                ),
+            }}
+            />
+
+            <Tab.Screen
+            name="Taller"
+            component={TallerProfileScreen}
+            options={{
+                title: "Taller",
+                tabBarIcon:({color, size}) => (
+                    <Ionicons name="construct" size={size} color={color}/>
+                ),
+            }}
+            />
+
+            
+        </Tab.Navigator>
+
+    )
+}
