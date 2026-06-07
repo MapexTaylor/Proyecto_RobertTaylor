4b05b887a1caecda56a7efda415a71237fa4b211
Robert Taylor <robert.taylor.mk@gmail.com>
2026-06-07 14:47:44 -0600

    - Creacion de primera pantalla, en esta pantalla se decide si es un cliente o un mecanico. - Se agrego funcion para navegar segun que boton se elija. Hasta el momento, no se ha agregado roles con context. - Si el boton "Ingresar como Taller" se presiona, se navegara a las tabs del taller, si el otro boton se presiona, se ira a la pantalla para buscar el proceso de su vehiculo.

---
+++ TallerCheck/src/screens/LoginScreen.tsx
@@ -0,0 +1,69 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import CustomButton from '../components/CustomButton';
+
+export default function LoginScreen({navigation}:any){
+
+    const handleLoginTaller = () => {
+        navigation.navigate("TallerLogin");
+    }
+
+    const handleLoginClient = () => {
+      navigation.navigate("ClientSearch")
+    }
+
+    return (
+    <View style={styles.container}>
+        <View style={styles.mainArea}>
+        
+        <Text style = {styles.text}>TallerCheck</Text>
+        <StatusBar style="auto" />
+
+    
+        <CustomButton 
+        title={'Ingresar como Taller'} 
+        onPress={handleLoginTaller}
+        variant='primary'/>
+
+        <CustomButton 
+        title={'Consultar como cliente'} 
+        onPress={handleLoginClient}
+        variant='primary'/>
+      </View>
+      <Image
+        source={require("../../assets/M.png")}
+        style={styles.image}
+        />
+    </View>
+    );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#868686',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mainArea:{
+    backgroundColor:'#eaeaea',
+    alignItems: 'center',
+    borderColor:'black',
+    borderWidth:2,
+    padding: 40,
+    paddingBottom:50,
+    borderRadius:20,
+    marginTop:150
+  },
+  text:{
+    paddingBottom:20,
+    fontWeight:'bold',
+    fontSize:30
+  },
+  image:{
+    width: 200,
+    height: 200,
+    resizeMode:"contain",
+    paddingTop:20
+  }
+});
