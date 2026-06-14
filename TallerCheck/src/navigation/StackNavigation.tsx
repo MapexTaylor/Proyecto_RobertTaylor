9de942cd63199f9425cc50a0d02682d5caab8bf1
Robert Taylor <robert.taylor.mk@gmail.com>
2026-06-14 13:18:19 -0600

    - Se agrego el role desde useAuth para que el stack navigator pueda mandar las pantallas segun el rol.

--- TallerCheck/src/navigation/StackNavigation.tsx
+++ TallerCheck/src/navigation/StackNavigation.tsx
@@ -4,6 +4,7 @@ import TabsNavigation from "./TabsNavigation";
 import ClientSearchScreen from "../screens/ClientSearchScreen";
 import TallerLoginScreen from "../screens/TallerLoginScreen";
 import TallerHomeScreen from "../screens/TallerHomeScreen";
+import { useAuth } from "../contexts/AuthContext";
 
 
 
@@ -17,12 +18,23 @@ export type RootStackParamList = {
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function StackNavigator(){
+    const {role} = useAuth();
     return(
         <Stack.Navigator initialRouteName="Login" screenOptions={{headerShown: false}}>
-            <Stack.Screen name="Login" component={LoginScreen}/>
-            <Stack.Screen name="UserTabs" component={TabsNavigation}/>
-            <Stack.Screen name="ClientSearch" component={ClientSearchScreen}/>
-            <Stack.Screen name="TallerLogin" component={TallerLoginScreen}/>
+            {role === null && (
+                <>
+                <Stack.Screen name="Login" component={LoginScreen}/>
+                <Stack.Screen name="TallerLogin" component={TallerLoginScreen}/>
+                </>
+            )}
+
+            {role === 'taller' && (
+                <Stack.Screen name="UserTabs" component={TabsNavigation}/>
+            )}
+            
+            {role === 'cliente' && (
+                <Stack.Screen name="ClientSearch" component={ClientSearchScreen}/>
+            )}
         </Stack.Navigator>
     )
 }
