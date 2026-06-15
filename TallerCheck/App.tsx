40cd62ff9ebb2399471dd60546632cb161e9d5f7
Robert Taylor <robert.taylor.mk@gmail.com>
2026-06-14 19:27:29 -0600

    - Se envolvio el ordercontext. - se modifico el ordersData porque ahora pide matricula de carro.

--- TallerCheck/App.tsx
+++ TallerCheck/App.tsx
@@ -2,13 +2,16 @@ import { NavigationContainer } from '@react-navigation/native';
 import StackNavigator from './src/navigation/StackNavigation';
 import { navigationRef } from './src/navigation/NavigationService';
 import { AuthProvider } from './src/contexts/AuthContext';
+import { OrdersProvider } from './src/contexts/OrdersContext';
 
 export default function App() {
   return (
     <AuthProvider>
-      <NavigationContainer ref={navigationRef}>
-        <StackNavigator></StackNavigator>
-      </NavigationContainer>
+      <OrdersProvider>
+        <NavigationContainer ref={navigationRef}>
+          <StackNavigator></StackNavigator>
+        </NavigationContainer>
+      </OrdersProvider>
     </AuthProvider>
   );
 }
