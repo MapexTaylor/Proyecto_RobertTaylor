625fa683b1fefc243e12039a668f6a1d3009e319
Robert Taylor <robert.taylor.mk@gmail.com>
2026-06-20 19:48:44 -0600

    - Se envolvió todo en el provider de la store.

--- TallerCheck/App.tsx
+++ TallerCheck/App.tsx
@@ -3,15 +3,20 @@ import StackNavigator from './src/navigation/StackNavigation';
 import { navigationRef } from './src/navigation/NavigationService';
 import { AuthProvider } from './src/contexts/AuthContext';
 import { OrdersProvider } from './src/contexts/OrdersContext';
+import { Provider } from 'react-redux';
+import { store } from './src/redux/store';
 
 export default function App() {
   return (
-    <AuthProvider>
-      <OrdersProvider>
-        <NavigationContainer ref={navigationRef}>
-          <StackNavigator></StackNavigator>
-        </NavigationContainer>
-      </OrdersProvider>
-    </AuthProvider>
+    <Provider store={store}>
+      <AuthProvider>
+        <OrdersProvider>
+          <NavigationContainer ref={navigationRef}>
+            <StackNavigator></StackNavigator>
+          </NavigationContainer>
+        </OrdersProvider>
+      </AuthProvider>
+    </Provider>
+    
   );
 }
