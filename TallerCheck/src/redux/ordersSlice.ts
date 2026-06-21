694b3f7799eee3b21b2cfb96b26b42264a296c4a
Robert Taylor <robert.taylor.mk@gmail.com>
2026-06-20 19:46:35 -0600

    - Creacion de folder redux y creacion de los hooks, odersSlice y el store. - Creacion de los dispatches.

---
+++ TallerCheck/src/redux/ordersSlice.ts
@@ -0,0 +1,55 @@
+import { ordersData } from "../data/ordersData";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+export type OrderStatus =
+  | "Recibido"
+  | "En diagnóstico"
+  | "En reparación"
+  | "Listo para entrega"
+  | "Entregado";
+
+export type Order = {
+  id: string;
+  code: string;
+  clientName: string;
+  phone: string;
+  marca: string;
+  matricula: string;
+  problem: string;
+  entryDate: string;
+  status: OrderStatus;
+};
+
+type OrdersState = {
+    orders: Order[];
+};
+
+const initialState: OrdersState = {
+    orders: ordersData as Order[],
+};
+
+const ordersSlice = createSlice({
+    name: "orders",
+    initialState,
+    reducers:{
+        addOrder: (state, action: PayloadAction<Order>) => {
+            state.orders.push(action.payload);
+        },
+
+        updateOrderStatus: (
+            state,
+            action: PayloadAction<{code: String; status: OrderStatus}>
+        ) => {
+            const orderFound = state.orders.find(
+                (order) => order.code === action.payload.code
+            );
+
+            if (orderFound) {
+                orderFound.status = action.payload.status;
+            }
+        },
+    },
+});
+
+export const { addOrder, updateOrderStatus } = ordersSlice.actions;
+export default ordersSlice.reducer;
